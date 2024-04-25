e1f06c3649b4addafb9b1e28569343a1a82ec788
I-yan-u <iyanuajimobi12@gmail.com>
2024-04-25 22:57:04 +0100

    Refactor GUI layout in app.py for system performance monitoring

--- app.py
+++ app.py
@@ -10,14 +10,23 @@ class App(customtkinter.CTk):
         self.geometry("500x400")
         self.title("System Performance")
 
-
+        # Init Frames
         self.frame1 = customtkinter.CTkFrame(self, width=150, height=350)
         self.frame2 = customtkinter.CTkFrame(self, width=380, height=350)
 
+        # Pack Frames
         self.frame1.pack(side="left", padx=5, pady=5)
         self.frame2.pack(side="right", padx=5, pady=5)
 
-        self.
+        # Init Buttons
+        self.button_b3 = customtkinter.CTkButton(self.frame1, text="Battery Info", command=None, height=30)
+        self.button_Cpu = customtkinter.CTkButton(self.frame1, text="CPU Usage", command=None, height=30)
+        self.button_Mem = customtkinter.CTkButton(self.frame1, text="Memory Usage", command=None, height=30)
+        
+        # Pack Buttons
+        self.button_b3.pack(padx=5, pady=60.25)
+        self.button_Cpu.pack(padx=5, pady=5)
+        self.button_Mem.pack(padx=5, pady=60.25)
 
 app = App()
 app.mainloop()
